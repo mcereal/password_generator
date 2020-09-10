10273f90227a4c6855f1e8e9a609a71ad4094d8d
Michael Curtis <mpbcurtis@gmail.com>
2020-09-10 12:36:21 -0400

    added function to display results inside html field

---
+++ index.js
@@ -0,0 +1,8 @@
+const genBtn = document.getElementById("genBtn");
+const pwdResult = document.getElementById("pwdResult");
+
+genBtn.addEventListener("click", testFunction);
+
+function testFunction() {
+  pwdResult.innerHTML = "test";
+}
