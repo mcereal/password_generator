0415eb035d59abb2a09760d24698f49b4049bb2a
Michael Curtis <mpbcurtis@gmail.com>
2020-09-12 19:08:09 -0400

    added alphabet upper and lowercase arrays

--- script.js
+++ script.js
@@ -11,3 +11,14 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+var alphabetUpperCase = [];
+var alphabetLowerCase = [];
+
+for (i = 65; i < 91; i++) {
+  var letter = String.fromCharCode(i);
+  alphabetUpperCase.push(letter);
+  alphabetLowerCase.push(letter.toLowerCase());
+}
+console.log(alphabetUpperCase);
+console.log(alphabetLowerCase);
