088448d5e835e7cb2cd85b61ab623bd94659ec5e
Michael Curtis <mpbcurtis@gmail.com>
2020-09-13 18:56:56 -0400

    more playing with key value pairs

--- script.js
+++ script.js
@@ -12,82 +12,48 @@ function writePassword() {
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
-// var unicodeSymbolSet1 = [33, 48];
-// var unicodeSymbolSet2 = [58, 65];
-// var unicodeSymbolSet3 = [91, 97];
-// var unicodeSymbolSet4 = [123, 127];
-// var alphabetUpperSet = [65, 91];
-// var alphabetLowerSet = [97, 123];
-// var numberSet = [48, 58];
-
-// var alphabetUpperCase = unicodeRetriever(alphabetUpperSet);
-// var alphabetLowerCase = unicodeRetriever(alphabetLowerSet);
-// var symbols = [];
-// var numbers = unicodeRetriever(numberSet);
-
-// var lowercaseSetting = true;
-// var uppercaseSetting = false;
-// var numberSetting = false;
-// var symbolSetting = false;
-
-// function randomNumber() {
-//   return Math.floor(Math.random() * characterSet.length);
-// }
-
-// returns an array of characters to be used to fill out character sets
-
-// function unicodeRetriever(unicodeSet) {
-//   var character = [];
-//   for (i = unicodeSet[0]; i < unicodeSet[1]; i++) {
-//     character.push(String.fromCharCode(i));
-//   }
-//   return character;
-// }
+// ranges of Unicode characters in decimal
 
 var unicodeCharacters = {
-  unicodeSymbolSet: [
+  symbolRange: [
     [33, 48],
     [58, 65],
     [91, 97],
     [123, 127],
   ],
-  alphabetUpperSet: [65, 91],
-  alphabetLowerSet: [97, 123],
-  numberSet: [48, 58],
+  alphabetUpperRange: [65, 91],
+  alphabetLowerRange: [97, 123],
+  numberRange: [48, 58],
 };
 
-function unicodeRetriever(unicodeSet) {
+// returns an array of unicode characters given a corresponding range in decimal
+
+function unicodeRetriever(decimalRange) {
   var character = [];
-  for (i = unicodeSet[0]; i < unicodeSet[1]; i++) {
+  for (i = decimalRange[0]; i < decimalRange[1]; i++) {
     character.push(String.fromCharCode(i));
   }
   return character;
 }
 
-// unicodeCharacters.unicodeSymbolSet.forEach((el) => {
-//   console.log(el);
-// });
+// parses key value pairs of unicode decimal ranges
 
-// for (let prop in unicodeCharacters) {
-//   console.log(unicodeCharacters[prop]);
-// }
-
-var alphabetUpperCase = [];
-var alphabetLowerCase = [];
-var symbols = [];
-var numbers = [];
-
-function characterSettings(characterSets) {
-  for (let key in characterSets) {
-    var test = [];
-    var test3 = test.concat(test2);
-    console.log(test3);
-    for (let value in characterSets[key]) {
-      // console.log(characterSets[key][value]);
-      var test2 = unicodeRetriever(characterSets[key][value]);
-      // console.log(unicodeRetriever(characterSets[key][value]));
-    }
+function characterSettings(characterRangeKey) {
+  var emptyArray = [];
+  var partialArray;
+  var characterArray = emptyArray.concat(partialArray);
+  for (let value in characterRangeKey) {
+    var partialArray = unicodeRetriever(value);
+    return partialArray;
   }
+  return characterArray;
 }
 
-characterSettings(unicodeCharacters);
+//arrays of possible characters to use in password generation
+
+var alphabetUpperCase = characterSettings(unicodeCharacters.alphabetUpperRange);
+var alphabetLowerCase = characterSettings(unicodeCharacters.alphabetLowerRange);
+var symbols = characterSettings(unicodeCharacters.symbolRange);
+var numbers = characterSettings(unicodeCharacters.numberRange);
+
+console.log(characterSettings(unicodeCharacters.symbolRange));
